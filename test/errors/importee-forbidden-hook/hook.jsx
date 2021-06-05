bd0d85f5e617069c00f44b21a8df875c00e8db7e
Flo <fpellet@ensc.fr>
2021-06-05 16:46:32 +0200

    full list of forbidden hooks in test

--- test/errors/importee-forbidden-hook/hook.jsx
+++ test/errors/importee-forbidden-hook/hook.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import {
+  useState,
+  useRef,
+  useReducer,
+  useContext,
+  createContext,
+} from "react";
+
+const Context = createContext(false)
+
 /**
  * @__IMPORTABLE_HOOK__
  */
-
 export default function useTestHook() {
   const [a, setA] = useState(false);
+  const b = useRef(false)
+  const [c, dispatchC] = useReducer(() => {}, false)
+  const d = useContext(Context)
 }
